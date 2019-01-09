4bb046b98d47b6930645a7ecee4063793a4aabe7
Prashant <prashant.badiger@cerner.com>
2019-01-09 17:23:50 +0530

    Completed below tasks: Added readme file added scripts to build npm package added .npmignore file

---
+++ README.md
@@ -0,0 +1,8 @@
+# react-js-circular-stepper
+
+When your form has multiple pages, a stepper is a must. Steppers keep users informed about their progress by indicating what step they’re on and how many steps are left.
+
+Displaying steppers on mobile forms is a challenge due to the limited screen space. Using the same stepper on your desktop form for your mobile form causes readability and visibility issues.
+
+This circulat stepper is more useful for Mobile Forms. It is completely responsive and it will fit in any size of mobile screen.
+
--- demo/components/desktop_view.jsx
+++ demo/components/desktop_view.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import CircularStepper from '../../src/circular_stepper';
 
-const steps = [{title: 'Contact Details', subtitle: 'Next: Shipping Information'}, 
-{title: 'Shipping Information', subtitle: 'Next: Billing Address'}, 
-{title: 'Billing Address', subtitle: 'Next: Payment & Review'},
-{title: 'Payment & Review', subtitle: ''}];
+const steps = [{title: 'Select Campaign Configuations', subtitle: 'Next: Create an Add Group'}, 
+{title: 'Create an Add Group', subtitle: 'Next: Create an Add'}, 
+{title: 'Create an Add', subtitle: 'Next: Preview Details'},
+{title: 'Preview Details', subtitle: ''}];
 
 export default class DesktopView extends React.Component {
     constructor(props){
@@ -33,13 +33,11 @@ export default class DesktopView extends React.Component {
                         steps={steps}
                         strokeWidth={10}
                         size={150} />
-                    <hr/>
                 </div>
-                <div >
+                <div className="desktop-body">
                     Hello, You are in <b>{steps[this.state.activeStep - 1].title} </b>step!
                 </div>
                 <div >
-                    <hr />
                     <input className="back" type="button" value="Back" onClick={this.state.activeStep != 1 ? this.handleOnClickBack : null} />
                     <input className="next" type="button" value="Next" onClick={this.state.activeStep === steps.length ? null : this.handleOnClickNext} />
                 </div>
